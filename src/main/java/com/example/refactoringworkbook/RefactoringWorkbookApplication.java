1c36cf7bbc696aea6495319809fabecbb2f2f5b7
garfield-kim <garfield.kim@kakaocorp.com>
2025-12-26 19:35:10 +0900

    init

---
+++ src/main/java/com/example/refactoringworkbook/RefactoringWorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.refactoringworkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringWorkbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringWorkbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/refactoringworkbook/RefactoringWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refactoringworkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringWorkbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
